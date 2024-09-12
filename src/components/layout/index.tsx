083de5690192e71de0dbcd746132416ad4a47b0c
dawngn <haidangdavid123@gmail.com>
2024-09-12 15:15:22 +0700

    feat: update

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -4,10 +4,10 @@ import Bottom from '../bottom'
 
 const MainLayout = () => {
   return (
-    <div className="h-[100dvh] w-screen overflow-hidden bg-black font-sf">
+    <div className="h-screen  w-screen overflow-hidden bg-black font-sf">
       <div className="mx-auto flex h-full w-full max-w-md flex-col bg-black">
         <div className="flex-1 overflow-y-auto bg-black">
-          <div className="h-[calc(100vh-64px)]">
+          <div className="">
             <Outlet />
           </div>
         </div>
